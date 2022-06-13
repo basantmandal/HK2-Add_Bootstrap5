eeadd8e2a09997f2ad2b531a3361247124bf6845
Basant (Kitto) <basantmandal@gmail.com>
2022-06-13 13:51:04 +0530

    Updated Doc Blocker

--- registration.php
+++ registration.php
@@ -1,20 +1,16 @@
 <?php
 /**
- * HK2
- * @author Basant Mandal - HK2 (HashTag Kitto)
- * Version 1.0.0
+ * Do not edit or add to this file if you wish to upgrade this extension to newer version in the future.
  *
- * DISCLAIMER
+ * In no event shall HK2 - Basant Mandal be liable for any damages (including, without limitation, lost profits, business interruption, or lost information) rising out of ‘Authorized Users’ use of or inability to use the HK2 - Basant Mandal products, even if HK2 - Basant Mandal has been advised of the possibility of such damages. In no event will HK2 - Basant Mandal be liable for prosecution arising from use of the Software against law or for any illegal use.
  *
- * Do not edit or add to this file if you wish to upgrade this extension to newer
- * version in the future.
  *
- * @category    HK2
+ * This module adds Bootstrap 5 Support in the Default or Active Theme. Bootstrap 5 is the newest version  of Bootstrap and is the world's most popular framework for building responsive, mobile-first sites. Bootstrap is an open-source framework that allows you to create responsive, professional-looking websites with ease.
+ * @version     1.0.0
  * @package     HK2_AddBootstrap5
  *
+ * @author Basant Mandal - HK2 (HashTag Kitto)
  * @copyright   Copyright (c) HK2 - Basant Mandal
- * @license     In no event shall HK2 - Basant Mandal be liable for any damages (including, without limitation, lost profits, business interruption, or lost information) rising out of ‘Authorized Users’ use of or inability to use the HK2 - Basant Mandal products, even if HK2 - Basant Mandal has been advised of the possibility of such damages. In no event will HK2 - Basant Mandal be liable for prosecution arising from use of the Software against law or for any illegal use.
  */
-
 
 \Magento\Framework\Component\ComponentRegistrar::register(\Magento\Framework\Component\ComponentRegistrar::MODULE, 'HK2_AddBootstrap5', __DIR__);
